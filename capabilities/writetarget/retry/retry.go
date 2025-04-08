721208f438824bccee676d3909dc688f6d04ab15
Silas Lenihan <32529249+silaslenihan@users.noreply.github.com>
2025-04-08 12:13:05 -0400

    Added common code needed for generalized WT (#36)
    * Added Generalized Write target
    
    Signed-off-by: Silas Lenihan <sjl@lenihan.net>
    
    * linting fixes
    
    * cleanup
    
    * extracted write target monitor from aptos
    
    * Refactored TargetStrategy methods to use request directly so they can decode their chain-specific configs from it
    
    * bumped go.mod
    
    * PR feedback
    
    * Refactored Execute transmission state check for all statuses
    
    * bump go.mod
    
    * moved beholder code from common to framework repo
    
    * linting fixes / ignores
    
    * added TODO
    
    * Added TODOs
    
    * addressed feedback
    
    * extracted report directory to chainlink-evm
    
    * removed beholder utils and retry back from common
    
    * linting fixes
    
    * Added protobuf ci
    
    * bump go.mod
    
    * implemented feedback
    
    * Extracted all EVM-related code
    
    * removed protobuf ci
    
    ---------
    
    Signed-off-by: Silas Lenihan <sjl@lenihan.net>

---
+++ capabilities/writetarget/retry/retry.go
@@ -0,0 +1,65 @@
+package retry
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jpillora/backoff"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+)
+
+type ctxKey string
+
+// ctxKeyID is the context key for tracing ID
+const ctxKeyID ctxKey = "retryID"
+
+func CtxWithID(ctx context.Context, retryID string) context.Context {
+	return context.WithValue(ctx, ctxKeyID, retryID)
+}
+
+// Exponential backoff (default) is used to handle retries with increasing wait times in case of errors
+var BackoffStrategyDefault = backoff.Backoff{
+	Min:    100 * time.Millisecond,
+	Max:    3 * time.Second,
+	Factor: 2,
+}
+
+// WithStrategy applies a retry strategy to a given function.
+func WithStrategy[R any](ctx context.Context, lggr logger.Logger, strategy backoff.Backoff, fn func(ctx context.Context) (R, error)) (R, error) {
+	// Generate a new tracing ID if not present, used to track retries
+	retryID := ctx.Value(ctxKeyID)
+	if retryID == nil {
+		retryID = uuid.New().String()
+		// Add the generated tracing ID to the context (as it was not already present)
+		ctx = context.WithValue(ctx, ctxKeyID, retryID)
+	}
+
+	// Track the number of retries
+	numRetries := int(strategy.Attempt())
+	for {
+		result, err := fn(ctx)
+		if err == nil {
+			return result, nil
+		}
+
+		wait := strategy.Duration()
+		message := fmt.Sprintf("Failed to execute function, retrying in %s ...", wait)
+		lggr.Warnw(message, "wait", wait, "numRetries", numRetries, "retryID", retryID, "err", err)
+
+		select {
+		case <-ctx.Done():
+			return result, fmt.Errorf("context done while executing function {retryID=%s, numRetries=%d}: %w", retryID, numRetries, ctx.Err())
+		case <-time.After(wait):
+			numRetries++
+			// Continue with the next retry
+		}
+	}
+}
+
+// With applies a default retry strategy to a given function.
+func With[R any](ctx context.Context, lggr logger.Logger, fn func(ctx context.Context) (R, error)) (R, error) {
+	return WithStrategy(ctx, lggr, BackoffStrategyDefault, fn)
+}
