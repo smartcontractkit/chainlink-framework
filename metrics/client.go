f41f83acf0796c130da41730fb50e14330d1a62b
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-04-14 11:17:40 -0400

    Add RPCCallLatency metric

--- metrics/client.go
+++ metrics/client.go
@@ -1,1 +1,25 @@
 package metrics
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	RPCCallLatency = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "rpc_call_latency",
+		Help: "The duration of an RPC call in nanoseconds",
+		Buckets: []float64{
+			float64(50 * time.Millisecond),
+			float64(100 * time.Millisecond),
+			float64(200 * time.Millisecond),
+			float64(500 * time.Millisecond),
+			float64(1 * time.Second),
+			float64(2 * time.Second),
+			float64(4 * time.Second),
+			float64(8 * time.Second),
+		},
+	}, []string{"ChainFamily", "ChainID", "rpcUrl", "isSendOnly", "success", "rpcCallName"})
+)
