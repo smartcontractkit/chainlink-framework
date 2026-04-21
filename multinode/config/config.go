0dc41d0bf7f90aec7acbf02b351b006a0142d725
Vasco Figueira <vasco.figueira@smartcontract.com>
2026-04-21 16:28:55 +0100

    fixup! DF-23489 unreachable node recovery depends on successful polls

--- multinode/config/config.go
+++ multinode/config/config.go
@@ -17,7 +17,6 @@ type MultiNode struct {
 
 	// Node Configs
 	PollFailureThreshold       *uint32
-	PollSuccessThreshold       *uint32
 	PollInterval               *config.Duration
 	SelectionMode              *string
 	SyncThreshold              *uint32
@@ -46,7 +45,7 @@ func (c *MultiNodeConfig) PollFailureThreshold() uint32 {
 }
 
 func (c *MultiNodeConfig) PollSuccessThreshold() uint32 {
-	return *c.MultiNode.PollSuccessThreshold
+	return 0 // retaining source compat for -solana; -evm sets via NodePoolConfig
 }
 
 func (c *MultiNodeConfig) PollInterval() time.Duration {
@@ -107,9 +106,6 @@ func (c *MultiNodeConfig) SetFrom(f *MultiNodeConfig) {
 	// Node Configs
 	if f.MultiNode.PollFailureThreshold != nil {
 		c.MultiNode.PollFailureThreshold = f.MultiNode.PollFailureThreshold
-	}
-	if f.MultiNode.PollSuccessThreshold != nil {
-		c.MultiNode.PollSuccessThreshold = f.MultiNode.PollSuccessThreshold
 	}
 	if f.MultiNode.PollInterval != nil {
 		c.MultiNode.PollInterval = f.MultiNode.PollInterval
